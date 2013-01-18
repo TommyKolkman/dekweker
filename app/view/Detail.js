fd47a491238740c2af7677781f411cc648187414
roydendro <roy_735@hotmail.com>
2013-01-18 10:47:27 +0100

    Added comments to Stylesheet. Added close button to the modal.

--- app/view/Detail.js
+++ app/view/Detail.js
@@ -4,6 +4,7 @@ Ext.define('dekweker.view.Detail', {
 
 	config:{
 		cls:'modal',
+		ui:'dark',
 		centered:true,
 		height: '90%',
 		width: '90%',
@@ -21,13 +22,32 @@ Ext.define('dekweker.view.Detail', {
 				this.destroy();
 			},
 			add:function(){
+				//Fire event to make the Controller set the appropriate scroll
 				this.fireEvent('setScroll', this);
+			},
+			initialize:function(){
+				//Add a close button and handler to the modal.
+				var _self=this;
+				var closeButton = Ext.create('Ext.Button', {
+					ui: 'action',
+					iconCls:'delete',
+					iconMask: true,
+					docked:'top',
+					cls:'closeButton',
+					handler:function(){
+						_self.destroy();
+					}
+				});
+				this.add(closeButton);
 			}
 		},
-		items:[{
+		items:[
+		{
 			id: 'folderImage',
-			html:'loading...'
-		}]
+			html:'Laden',
+			cls:'detailview'
+		}
+		]
 		
 	}
 
