ad0203878912ba7e114007dd73c6db83121c4db6
roydendro <roy_735@hotmail.com>
2013-02-01 11:58:21 +0100

    Fixed the most stupid mistake ever.

--- android/src/com/ZwaarContrast/DeKweker/DeKwekerApplication.java
+++ android/src/com/ZwaarContrast/DeKweker/DeKwekerApplication.java
@@ -12,18 +12,24 @@ public class DeKwekerApplication extends Application {
  
     @Override
     public void onCreate() {
+        //Load options
     	AirshipConfigOptions options = AirshipConfigOptions.loadDefaultOptions(this);
-    	options.transport = "helium";
-    			options.developmentAppKey = "_7UGKHwnQ2eKFLNNy_pgxw";
-    			options.developmentAppSecret = "C5n_fo9fQXixZWPVCGJqRg";
-    			options.productionAppKey = "";
-    			options.productionAppSecret = "";
-    			options.inProduction = false;
+    	
+        //Set options to enable push notifications
+        options.transport = "helium";
+		options.developmentAppKey = "_7UGKHwnQ2eKFLNNy_pgxw";
+		options.developmentAppSecret = "C5n_fo9fQXixZWPVCGJqRg";
+		options.productionAppKey = "5wioYYXrS3eykPlh-dxwPg";
+		options.productionAppSecret = "C2czYq-wQIqUpQKuQF8i-A";
+		options.inProduction = true;
+        
+        //Up up and away
         UAirship.takeOff(this,options);
  
+        //Enable push
         PushManager.enablePush();
         
-
+        //Get APID
         PushPreferences prefs = PushManager.shared().getPreferences();
         Logger.info("My Application onCreate - App APID: " + prefs.getPushId());
     }
